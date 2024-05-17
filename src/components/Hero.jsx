1c2dbd44b007ed454f9e2bb4d1ceff2ac8606063
Andrea DAngelo <dangeloandrea1999@gmail.com>
2024-05-17 14:10:15 +0200

    check mobile usage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,25 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+    return "unknown";
+  }
+  
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -18,7 +37,7 @@ const Hero = () => {
           </span></h1>
 
           <p>I'm an XR Developer and a CS student, 
-          <br className="sm:block hidden"/> with an interest for Game Design and VR systems.
+          <br className="sm:block hidden"/> with an interest <i><b>{getMobileOperatingSystem()}</b></i> for Game Design and VR systems.
           </p>
         </div>
       </div>
